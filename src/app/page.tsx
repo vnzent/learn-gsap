44af770e135ade3b154289da4a7ff78b56423234
Vincent <vincent.ptk17@gmail.com>
2025-10-03 16:54:11 +0800

    import homepage and about to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
+import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
 
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <HomePage />
+      <About />
     </>
   );
 }
