ba464d703626b2f1889f5ade903013bfe6dece07
Vincent <vincent.ptk17@gmail.com>
2025-10-03 16:53:12 +0800

    add HomePage and add video as the content

---
+++ src/app/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <section id="home" className="h-screen">
+      <video autoPlay muted loop className="h-full object-cover" src="https://videos.pexels.com/video-files/4069480/4069480-uhd_2560_1440_25fps.mp4"></video>
+    </section>
+  );
+}
+
+export default HomePage
