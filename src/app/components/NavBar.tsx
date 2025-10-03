994d017aa822ded95f65b7980084fa5d78226ea8
Vincent <vincent.ptk17@gmail.com>
2025-10-03 09:24:35 +0800

    feat: create a navbar transition with gsap

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import Link from "next/link";
+import gsap from "gsap";
+import { useEffect } from "react";
+
+const NavBar = () => {
+  const tl = gsap.timeline({ reversed: true, paused: true });
+
+  useEffect(() => {
+    const lineOne = document.querySelector(".line-one");
+    const lineTwo = document.querySelector(".line-two");
+    const menuWrap = document.querySelector(".menu-wrap")!.getClientRects();
+    const widthOne = lineOne!.getClientRects()[0];
+    const widthTwo = lineTwo!.getClientRects()[0];
+
+    console.log(widthOne);
+
+    const gap = menuWrap[0].height - widthOne.height - widthTwo.height;
+    console.log({ gap });
+    console.log(menuWrap);
+    tl.to(".sidebar", { clipPath: "circle(150% at 0% 0%" }, "<")
+      .to(
+        ".line-one",
+        {
+          transformOrigin: "left top",
+          rotation: "15%",
+        },
+        "<"
+      )
+      .to(
+        ".line-two",
+        {
+          transformOrigin: "left bottom",
+          rotation: "-15%",
+        },
+        "<"
+      )
+      .to(".nav-link", { y: 0, duration: 0.5, stagger: 0.05 }, "<")
+      .to(".nav-img", { clipPath: "inset(0 0% 0 0)", scale: 1 }, "<");
+  }, []);
+
+  const menus = [
+    {
+      label: "Home",
+      url: "#home",
+    },
+    {
+      label: "About",
+      url: "#about",
+    },
+    {
+      label: "Content",
+      url: "#content",
+    },
+    {
+      label: "Sub Content",
+      url: "#sub-content",
+    },
+    {
+      label: "Contact",
+      url: "#contact",
+    },
+  ];
+
+  const handleSideBar = () => {
+    if (tl.reversed()) {
+      tl.play();
+    } else {
+      tl.reverse();
+    }
+  };
+
+  return (
+    <div className="relative h-screen">
+      <div
+        onClick={handleSideBar}
+        className="absolute menu-wrap flex flex-col gap-2 top-10 left-10 cursor-pointer z-999"
+      >
+        <span className="line-one inline-block bg-red-500 w-10 h-0.5" />
+        <span className="line-two inline-block bg-red-500 w-10 h-0.5" />
+      </div>
+      <nav
+        className="sidebar bg-amber-800 w-full h-full flex items-center"
+        style={{ clipPath: "circle(0% at 0 0)" }}
+      >
+        <div className="flex w-1/2 p-10">
+          <ul className="flex flex-col gap-7 text-3xl font-semibold text-white">
+            {menus.map((menu, index) => (
+              <li
+                key={index}
+                className="overflow-hidden relative h-10 w-100 flex"
+              >
+                <Link
+                  href={menu.url}
+                  className="nav-link absolute h-10 translate-y-[100%]"
+                >
+                  {menu.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-1/2 h-full overflow-hidden">
+          <img
+            className="nav-img w-full h-full object-cover scale-150"
+            src="./assets/nav.jpeg"
+            alt="resort"
+            style={{ clipPath: "inset(0 100% 0 0)" }}
+          />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
