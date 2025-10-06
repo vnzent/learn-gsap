8740090c6c6a863b1b1abcdd5eb83da404ce9ed4
Vincent <vincent.ptk17@gmail.com>
2025-10-06 17:07:55 +0800

    create new section named SubContent.tsx and import in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HomePage from "./components/HomePage";
 import About from "./components/About";
 import Content from "./components/Content";
+import SubContent from "./components/SubContent";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HomePage />
       <About />
       <Content />
+      <SubContent />
     </>
   );
 }
